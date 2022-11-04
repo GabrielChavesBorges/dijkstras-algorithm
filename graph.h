a84e88151d964cc1fa4cc2def8d37e43cba66b6d
Gabriel Borges <gabriel.chaves.borges@gmail.com>
2022-11-04 23:11:43 +0900

    add input file

--- graph.h
+++ graph.h
@@ -1,13 +1,7 @@
 #ifndef GRAPH_H
 #define GRAPH_H
-#include <stdbool.h>
 
 char * test(void);
-
-struct Vertex {
-    int id;
-    bool wasVisited;
-};
 
 char * test(void) {
     return "hi there";
--- main.c
+++ main.c
@@ -1,7 +1,30 @@
 #include <stdio.h>
-#include "graph.h"
+#include <stdbool.h>
+// #include "graph.h"
 
-int main() {
-    printf(test());
+typedef struct Node_type
+{
+    int id;
+    bool was_visited;
+    int tentative_distance;
+    int *neighbor_nodes;
+} Node;
+
+int main()
+{
+    // read a file with inputs
+    FILE *file_pointer;
+    int n_nodes;
+    int n_edges;
+    int i;
+
+    file_pointer = fopen("./input.dat", "r");
+    fscanf(file_pointer, "%d %d\n", &n_nodes, &n_edges);
+
+    for (i = 0; i < n_edges; i++) {
+        // get edges
+    }
+    printf("%d %d", n_nodes, n_edges);
+    fclose(file_pointer);
     return 0;
 }
